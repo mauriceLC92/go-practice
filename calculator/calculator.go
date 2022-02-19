1fc48c9929d905a3edb2f035aa9801eadd2ae1fa
Maurice Le Cordier <mauricelecordier@gmail.com>
2022-02-19 18:17:32 +0200

    Adds the substraction function

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -10,3 +10,14 @@ func Add(inputs ...float64) float64 {
 	}
 	return res
 }
+
+func Subtract(inputs ...float64) float64 {
+	if len(inputs) == 0 {
+		return 0
+	}
+	res := inputs[0]
+	for _, in := range inputs[1:] {
+		res -= in
+	}
+	return res
+}
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,12 +6,47 @@ import (
 	"github.com/mauriceLC92/go-practice/calculator"
 )
 
+type testCase struct {
+	inputs []float64
+	want   float64
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 18
-	got := calculator.Add(5, 6, 7)
 
-	if want != got {
-		t.Errorf("Add: want %f but got %f", want, got)
+	testCases := []testCase{
+		{inputs: []float64{1, 2, 3}, want: 6},
+		{inputs: []float64{-1, -2, -3}, want: -6},
+		{inputs: []float64{}, want: 0},
+		{inputs: []float64{10, 2, 33, 7, -5}, want: 47},
+	}
+
+	for _, test := range testCases {
+		want := test.want
+		got := calculator.Add(test.inputs...)
+		if want != got {
+			t.Errorf("Add(%v): want %f, got %f",
+				test.inputs, want, got)
+		}
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{inputs: []float64{1, 2, 3}, want: -4},
+		{inputs: []float64{-1, -2, -3}, want: 4},
+		{inputs: []float64{}, want: 0},
+		{inputs: []float64{10, 2, 33, 7, -5}, want: -27},
+	}
+
+	for _, test := range testCases {
+		want := test.want
+		got := calculator.Subtract(test.inputs...)
+		if want != got {
+			t.Errorf("Subtract(%v): want %f, got %f",
+				test.inputs, want, got)
+		}
 	}
 }
