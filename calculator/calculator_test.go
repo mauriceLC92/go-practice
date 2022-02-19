5d19d32825f1e4a3b99ec9833aaaa1f8fb26d120
Maurice Le Cordier <mauricelecordier@gmail.com>
2022-02-19 18:03:49 +0200

    Add function plus test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -1,1 +1,17 @@
 package calculator_test
+
+import (
+	"testing"
+
+	"github.com/mauriceLC92/go-practice/calculator"
+)
+
+func TestAdd(t *testing.T) {
+	t.Parallel()
+	var want float64 = 18
+	got := calculator.Add(5, 6, 7)
+
+	if want != got {
+		t.Errorf("Add: want %f but got %f", want, got)
+	}
+}
