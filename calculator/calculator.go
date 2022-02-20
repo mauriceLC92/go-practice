cebfb547e5d00c41651ddb653f748f3fa6909408
Maurice Le Cordier <mauricelecordier@gmail.com>
2022-02-20 14:13:57 +0200

    add multiply and divide functions with their corresponding tests

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "errors"
+
 func Add(inputs ...float64) float64 {
 	if len(inputs) == 0 {
 		return 0
@@ -20,4 +22,24 @@ func Subtract(inputs ...float64) float64 {
 		res -= in
 	}
 	return res
+}
+
+func Multiply(inputs ...float64) float64 {
+	if len(inputs) == 0 {
+		return 0
+	}
+
+	res := inputs[0]
+	for _, in := range inputs {
+		res *= in
+	}
+	return res
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("no allowed to divide by zero")
+	}
+
+	return a / b, nil
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -50,3 +50,56 @@ func TestSubtract(t *testing.T) {
 		}
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+
+	testCases := []testCase{
+		{inputs: []float64{1, 2, 3}, want: 6},
+		{inputs: []float64{-1, -2, 0}, want: 0},
+		{inputs: []float64{}, want: 0},
+		{inputs: []float64{10, 2, 33, 7, -5}, want: -231000},
+	}
+
+	for _, test := range testCases {
+		want := test.want
+		got := calculator.Multiply(test.inputs...)
+		if want != got {
+			t.Errorf("Multiply(%v): want %f, got %f",
+				test.inputs, want, got)
+		}
+	}
+}
+
+func TestDivideInvalid(t *testing.T) {
+	var numerator float64 = 6
+	var denominator float64 = 0
+
+	_, err := calculator.Divide(numerator, denominator)
+
+	if err == nil {
+		t.Errorf("wanted error for invalid input but got nil instead")
+	}
+}
+
+func TestDivide(t *testing.T) {
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+
+	testCases := []testCase{
+		{a: 4, b: 1, want: 4},
+		{a: 2, b: 2, want: 1},
+		{a: 10, b: 2, want: 5},
+	}
+
+	for _, test := range testCases {
+		want := test.want
+		got, _ := calculator.Divide(test.a, test.b)
+
+		if want != got {
+			t.Errorf("Divide(%f,%f): want %f, got %f", test.a, test.b, want, got)
+		}
+	}
+}
