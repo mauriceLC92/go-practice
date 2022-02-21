b17ed2c34b93f4862b11a6f88c3d7db4e5898613
Maurice Le Cordier <mauricelecordier@gmail.com>
2022-02-21 20:44:41 +0200

    some funky logic that does not calculate the correct answer

--- calculator/cmd/main.go
+++ calculator/cmd/main.go
@@ -1,8 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/mauriceLC92/go-practice/calculator"
+)
+
+const (
+	Add      string = "add"
+	Subtract string = "subtract"
+	Divide   string = "divide"
+	Multply  string = "multiply"
+)
+
+func convertInputString(input []string) ([]float64, error) {
+	fmt.Printf("%d", len(input))
+	res := make([]float64, len(input))
+
+	for i, in := range input {
+		output, err := strconv.ParseFloat(in, 64)
+
+		if err != nil {
+			return nil, fmt.Errorf("error converting %f to float64 value", output)
+		}
+		res[i] = output
+	}
+	return res, nil
+}
+
+func printAnswer(ans float64, inputs []string, operation string) {
+	fmt.Printf("%s(%s) = %f", operation, inputs, ans)
+}
 
 func main() {
+	operation := flag.String("operation", Add, "the calculator function to use")
+	flag.Parse()
 
-	fmt.Println("Hello")
+	values := flag.Args()
+	valArr := strings.Split(values[0], ",")
+	res, err := convertInputString(valArr)
+	if err != nil {
+		fmt.Printf("error converting input into float4 values: %s", err)
+		os.Exit(1)
+	}
+
+	switch *operation {
+	case Add:
+		res := calculator.Add(res...)
+		printAnswer(res, values, Add)
+	}
 }
